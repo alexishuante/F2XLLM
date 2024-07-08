331ecbf78a74f59cd40b086840aef0e36e2ad975
Gonzalez J <jvalglz@login.excl.ornl.gov>
2024-07-08 14:17:18 -0400

    AXPY testing performance records

--- axpy/OpenACC.f90
+++ axpy/OpenACC.f90
@@ -1,23 +1,3 @@
-<<<<<<< HEAD
-! PROGRAM main
-!     use openacc
-!     IMPLICIT NONE
-!     INTEGER, PARAMETER :: n = 500000
-!     REAL, DIMENSION(n) :: x, y
-!     REAL :: a
-!     INTEGER :: i
-
-!     ! Initialize the scalar and vectorss
-!     a = 2.0
-!     x = 3.0
-!     y = 0.0
-
-!     ! Call the saxpy subroutine
-!     DO i = 1, 500000
-!         CALL saxpy(n, a, x, y)
-!     END DO
-!     CALL saxpy(n, a, x, y)
-=======
 ! Test Comments test
 PROGRAM main
     USE iso_c_binding, ONLY: c_int, c_float
@@ -45,7 +25,6 @@ PROGRAM main
 
     ! Call the C wrapper instead of the Fortran subroutine
     CALL saxpy_wrapper(n, a, x, y)
->>>>>>> 754568a871de67204395c30e11839c948ec2d7a9
 
 ! END PROGRAM main
 
@@ -63,26 +42,4 @@ PROGRAM main
 !     end do
 !     !$acc end data
 
-<<<<<<< HEAD
-! end subroutine saxpy
-! ! Test Comments test
-
-program vector_add
-    use openacc 
-    implicit none
-    integer, parameter :: N = 1000000
-    real :: a(N), b(N), c(N)
-  
-    ! Initialize arrays
-    a = 1.0
-    b = 2.0
-  
-    !$acc parallel loop
-    do i = 1, N
-      c(i) = a(i) + b(i)
-    end do
-    !$acc end parallel loop
-  end program vector_add
-=======
 ! Remove the Fortran saxpy subroutine
->>>>>>> 754568a871de67204395c30e11839c948ec2d7a9
